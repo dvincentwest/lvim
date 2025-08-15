9220aa6ceacd06267fcf514c49a4c408dc24e3d1
Vince West <vince_west@apple.com>
2025-08-15 11:14:54 -0500

    trim whitespace on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+
+-- Trim trailing whitespace and make exactly one newline
+local function trim_whitespace_and_trailing_newlines()
+  local save_cursor = vim.fn.getpos(".")
+  -- Remove all trailing whitespace
+  vim.cmd([[%s/\s\+$//e]])
+  -- Remove all trailing newlines
+  vim.cmd([[%s/\n\+\%$//e]])
+  -- Add exactly one newline at the end
+  vim.cmd([[%s/\%$/\r/e]])
+  vim.fn.setpos(".", save_cursor)
+end
+
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("trim_whitespace", { clear = true }),
+  pattern = "*",
+  callback = trim_whitespace_and_trailing_newlines,
+})
